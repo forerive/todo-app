1ed9e5f176c92d4754ae8109c99a4f3f90cf90ec
forerive <fore.rive.job.h@gmail.com>
2026-07-12 16:24:11 +0900

    feat: implement get todos API

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { TodoService } from './todo.service';
+
+@Controller('todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+  @Get()
+  findAll() {
+    return this.todoService.findAll();
+  }
+}

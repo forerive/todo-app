838d02ab1660344490679f0b1f7d2effd3d518df
forerive <fore.rive.job.h@gmail.com>
2026-07-18 10:29:40 +0900

    feat: implement create todo API

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Injectable()
 export class TodoService {
   findAll() {
     return [];
+  };
+
+  create(createTodoDto: CreateTodoDto) {
+    return {
+      id: 1,
+      title: createTodoDto.title,
+      completed: false,
+    };
   }
 }

cb855683ef289d9d62d32f06a1b8e7cb3a9cf57c
forerive <fore.rive.job.h@gmail.com>
2026-07-20 15:01:30 +0900

    feat: postgres導入

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todo/todo.entity';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'todo',
+      entities: [Todo],
+
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/todo/todo.entity.ts
+++ src/todo/todo.entity.ts
@@ -1,5 +1,12 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+@Entity()
 export class Todo {
+  @PrimaryGeneratedColumn()
   id!: number;
+
+  @Column()
   title!: string;
+
+  @Column()
   completed!: boolean;
 }

ab9f9c41d90c9d238379ebf72ce433646a3d9b31
forerive <fore.rive.job.h@gmail.com>
2026-07-21 22:06:33 +0900

    validation追加

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Todo API')
@@ -17,4 +20,4 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,3 +1,7 @@
+import { IsString, Length } from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString()
+  @Length(1, 50)
   title!: string;
 }
